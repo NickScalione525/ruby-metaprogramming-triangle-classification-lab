9a961d2fbeb442c8e15280d323feca397db1cc44
nick <nicholas_scalione@yahoo.com>
2021-01-28 00:43:58 -0500

    commit tree c077d52b04337fe0ea6af2a8908c5c3f405f94ab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+
+def kind
+  if @side_a + @side_b <= @side_c  || @side_a >= @side_b + @side_c || @side_a + @side_c <= @side_b ||  @side_a <= 0 
+    raise TriangleError
+
+
+  else 
+    if @side_a == @side_b && @side_b == @side_c
+      :equilateral
+    elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
+      :isosceles
+    elsif (@side_a != @side_b || @side_c) || (@side_b != @side_c)
+    :scalene
+   
+    end
+  end
+end
+class TriangleError < StandardError
+  end
+
 end
